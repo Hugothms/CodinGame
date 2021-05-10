17ce4eacecf4360579489544867470e5db1eae5d
Hugothms <hugothms@gmail.com>
2021-05-10 07:49:15 +0200

    add default C++

---
+++ codinggame.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int numberOfCells; // 37
+    cin >> numberOfCells; cin.ignore();
+    for (int i = 0; i < numberOfCells; i++) {
+        int index; // 0 is the center cell, the next cells spiral outwards
+        int richness; // 0 if the cell is unusable, 1-3 for usable cells
+        int neigh0; // the index of the neighbouring cell for each direction
+        int neigh1;
+        int neigh2;
+        int neigh3;
+        int neigh4;
+        int neigh5;
+        cin >> index >> richness >> neigh0 >> neigh1 >> neigh2 >> neigh3 >> neigh4 >> neigh5; cin.ignore();
+    }
+
+    // game loop
+    while (1) {
+        int day; // the game lasts 24 days: 0-23
+        cin >> day; cin.ignore();
+        int nutrients; // the base score you gain from the next COMPLETE action
+        cin >> nutrients; cin.ignore();
+        int sun; // your sun points
+        int score; // your current score
+        cin >> sun >> score; cin.ignore();
+        int oppSun; // opponent's sun points
+        int oppScore; // opponent's score
+        bool oppIsWaiting; // whether your opponent is asleep until the next day
+        cin >> oppSun >> oppScore >> oppIsWaiting; cin.ignore();
+        int numberOfTrees; // the current amount of trees
+        cin >> numberOfTrees; cin.ignore();
+        for (int i = 0; i < numberOfTrees; i++) {
+            int cellIndex; // location of this tree
+            int size; // size of this tree: 0-3
+            bool isMine; // 1 if this is your tree
+            bool isDormant; // 1 if this tree is dormant
+            cin >> cellIndex >> size >> isMine >> isDormant; cin.ignore();
+        }
+        int numberOfPossibleActions; // all legal actions
+        cin >> numberOfPossibleActions; cin.ignore();
+        for (int i = 0; i < numberOfPossibleActions; i++) {
+            string possibleAction;
+            getline(cin, possibleAction); // try printing something from here to start with
+        }
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+
+        // GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
+        cout << "WAIT" << endl;
+    }
+}
